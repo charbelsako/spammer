d845c9874ff368f7e6a7d1318e640211b0413301
Charbel Sarkis <charbelsako@gmail.com>
2019-03-08 12:04:49 +0200

    enhanced the config file. removed unnecessary variables. fixed comments.

--- config.py
+++ config.py
@@ -1,11 +1,21 @@
 # change these variables where needed
-URL = 'https://web.whatsapp.com'
+CONTACT_NAME = 'Melissa'
+# number of messages to send to the contact
+NUM_MSGS = 10
+# if set to False, send these messages instead of random text
+# not implemented yet
+# TODO: refactor
+USE_RANDOM_TEXT = True
+MESSAGES = ['USER', 'DEFINED', 'MESSAGES']
+# the browser that you use
+BROWSER_NAME = 'Firefox'
 
 # browser info
-BROWSER_NAME = 'Firefox'
-# the profile allows the app to keep the user logged and use previously opened sessions.
+# the profile allows the app to keep the user logged in and use previously opened sessions.
 CHROME_DATA_DIR = ''
+# change to your directory
 FIREFOX_PROFILE = '/home/charbelsako/.mozilla/firefox/'
+# change to your profile name
 PROFILE_NAME = 'm2y35oha.default'
 
 if BROWSER_NAME == 'Chrome'.lower():
@@ -13,22 +23,9 @@ if BROWSER_NAME == 'Chrome'.lower():
 else:
     PROFILE = ''.join([FIREFOX_PROFILE, PROFILE_NAME])
 
-CONTACT_NAME = 'Markos'
-# number of messages to send to the contact
-NUM_MSGS = 10
-
-# if set to True send these messages instead of random text
-USE_RANDOM_TEXT = False
-MESSAGES = ['USER', 'DEFINED', 'MESSAGES']
-
-# the activator has to have a class of focused so you can type your message
-ACTIVATOR_CLASS_NAME = '_3F6QL'
-# the input field. although this is a div
-INPUT_CLASS_NAME = '_2S1VP'
+URL = "https://web.whatsapp.com"
 # the contact card that you can click on
-CONTACT_CLASS_NAME = '_3j7s9'
-# contact name class
-CONTACT_NAME_CLASS = '_1wjpf'
-CONTACT_NAME_PARENT_CLASS = '_3TEwt'
-
+CONTACT_CLASS_NAME = "_3j7s9"
+# contact name class. Could be obsolete
+CONTACT_NAME_CLASS = "_1wjpf"
 SEND_BUTTON_CLASS = "_35EW6"
