995824663a1bb97b106914d234864c6baa87c88b
Charbel Sarkis <charbelsako@gmail.com>
2019-03-11 15:50:06 +0200

    deleted useless files

--- inputy.py
+++
@@ -1,5 +0,0 @@
-from pyautogui import hotkey
-
-# press('a')
-# typewrite('quick brown fox')
-hotkey('alt', 'tab')
--- utils.py
+++
@@ -1,6 +0,0 @@
-# never tested. Useless file
-def get_parent(elt, n_iter):
-    current = elt
-    for i in range(n_iter):
-        parent = current.find_element_by_xpath("..")
-        current = parent
